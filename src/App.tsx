fbe873236442934e0afc63c8c2a5608259036352
Katopuk <ladno4ka@gmail.com>
2022-12-21 20:36:14 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import { Button } from './components/button/Button';
+import { Input } from './components/input/Input'
 
 function App() {
+  const [startValue, setStartValue] = useState<number>(0)
+  const [endValue, setEndValue] = useState<number>(5)
+  const [count, setCount] = useState<number>(startValue)
+
+  const callBackInc = () => {
+    setCount(count + 1)
+  }
+
+  const callBackReset = () => {
+    setCount(startValue)
+  }
+  const callBackSet = () => {
+    setCount(startValue)
+  }
+
+  const callBackInputMax = (maxValue: number) => {
+    setEndValue(maxValue)
+  }
+
+  const callBackInputStart = (startValue: number) => {
+    setStartValue(startValue)
+  }
+
+  const disabledButtonInc = endValue > count ? false : true
+  const disabledButtonReset = count !== startValue ? false : true
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={'block-counter'}>
+        <div className={'block-input'}>
+          <Input title={'max value'} value={endValue} callBack={callBackInputMax} />
+          <Input title={'start value'} value={startValue} callBack={callBackInputStart} />
+        </div>
+        <div className={'block-buttons'}>
+          <Button title={'set'} callBack={callBackSet} />
+
+        </div>
+
+      </div>
+
+      <div className={'block-counter'}>
+        <div className={'block-input'}>
+          {count}
+        </div>
+        <div className={'block-buttons'}>
+          <Button title={'inc'} callBack={callBackInc} disabled={disabledButtonInc} />
+          <Button title={'reset'} callBack={callBackReset} disabled={disabledButtonReset} />
+        </div>
+
+      </div>
     </div>
   );
 }
